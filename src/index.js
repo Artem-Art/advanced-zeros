72d1fb448a4b1e17f845d94e2181a68167dc9614
artfront <krivonosart19@gmail.com>
2019-03-24 20:05:52 +0300

    solve the task in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let n = number;
+    let b = base;
+    let objPrim = {};
+    let res = 0;
+    let resArr = [];
+    for (let i = 2; i <= b; i++) {
+        if (b % i == 0) {
+            objPrim[i] = 0;
+            while (b % i == 0) {
+                objPrim[i] += 1;
+                b /= i;
+            }
+        }
+    }
+    for (let key in objPrim) {
+        let count = key;
+        while (count <= n) {
+            res += Math.floor(n / count);
+            count = count * key;
+        }
+        resArr.push(Math.floor(res / objPrim[key]));
+        res = 0;
+    }
+    return Math.min(...resArr);
 }
+
